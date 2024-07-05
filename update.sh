466092df57679cff44c885d27f0cb7f931dc6df2
root <tanakamasayuki@gmail.com>
2024-07-05 10:46:50 +0900

    update

--- update.sh
+++ update.sh
@@ -8,26 +8,26 @@ git pull
 # https://www.wch-ic.com/products/CH32V307.html
 cd datasheet_en
 # https://www.wch-ic.com/downloads/CH32V20x_30xDS0_PDF.html
-wget --continue https://www.wch-ic.com/downloads/file/325.html -O CH32V20x_30xDS0.PDF
+curl -z CH32V20x_30xDS0.PDF -o CH32V20x_30xDS0.PDF https://www.wch-ic.com/downloads/file/325.html
 # https://www.wch-ic.com/downloads/CH32V307DS0_PDF.html
-#wget --continue https://www.wch-ic.com/downloads/file/356.html -O CH32V307DS0.PDF #(skip) Same file as CH32V20x_30xDS0.PDF
+#curl -z CH32V307DS0.PDF -o CH32V307DS0.PDF https://www.wch-ic.com/downloads/file/356.html #(skip) Same file as CH32V20x_30xDS0.PDF
 # https://www.wch-ic.com/downloads/CH32FV2x_V3xRM_PDF.html
-wget --continue https://www.wch-ic.com/downloads/file/324.html -O CH32FV2x_V3xRM.PDF
+curl -z CH32FV2x_V3xRM.PDF -o CH32FV2x_V3xRM.PDF https://www.wch-ic.com/downloads/file/324.html
 cd ..
 
 # https://www.wch.cn/products/CH32V303.html
 # https://www.wch.cn/products/CH32V307.html
 cd datasheet_zh
 # https://www.wch.cn/downloads/CH32V20x_30xDS0_PDF.html
-wget --continue https://www.wch.cn/downloads/file/355.html -O CH32V20x_30xDS0.PDF
+curl -z CH32V20x_30xDS0.PDF -o CH32V20x_30xDS0.PDF https://www.wch.cn/downloads/file/355.html
 # https://www.wch.cn/downloads/CH32V307DS0_PDF.html
-#wget --continue https://www.wch.cn/downloads/file/416.html -O CH32V307DS0.PDF #(skip)  Same file as CH32V20x_30xDS0.PDF
+#curl -z CH32V307DS0.PDF -o CH32V307DS0.PDF https://www.wch.cn/downloads/file/416.html #(skip)  Same file as CH32V20x_30xDS0.PDF
 # https://www.wch.cn/downloads/CH32FV2x_V3xRM_PDF.html
-wget --continue https://www.wch.cn/downloads/file/371.html -O CH32FV2x_V3xRM.PDF
+curl -z CH32FV2x_V3xRM.PDF -o CH32FV2x_V3xRM.PDF https://www.wch.cn/downloads/file/371.html
 cd ..
 
 # https://www.wch.cn/downloads/CH32V307EVT_ZIP.html
-wget --continue https://www.wch.cn/downloads/file/356.html -O CH32V307EVT.ZIP
+curl -z CH32V307EVT.ZIP -o CH32V307EVT.ZIP https://www.wch.cn/downloads/file/356.html
 rm -rfv EVT
 unzip -O GB2312 *.ZIP
 
